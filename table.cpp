dba1b64256503fbc46de9bff8ef15b39d774242c
rrkwxlcvhizd <rrkwxlcvhizd@gmail.com>
2025-02-07 23:45:21 +0800

    Modify table.cpp

--- table.cpp
+++ table.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Receive officer bad research.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
